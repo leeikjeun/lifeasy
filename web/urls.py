8d55357f8f5531cd706227dd0eaf0207351d29ed
leeikjeun <dldlrwjs12@gmail.com>
2017-06-03 17:05:30 +0900

    change design html form

--- web/urls.py
+++ web/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.firstPage, name='first'),
-    url(r'^index$', views.mainPage, name='index'),
+    url(r'^$', views.mainPage, name='index'),
 ]
--- web/views.py
+++ web/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def firstPage(request):
-    return render(request, 'web/firstPage.html',{})
-
 def mainPage(request):
     return render(request, 'web/index.html',{})
 
