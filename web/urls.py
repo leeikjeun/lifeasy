1144084dee0c2df931d1c5405cf27a6793986f7a
ShelcodeSniper <shellcodesniper@gmail.com>
2017-06-13 19:07:15 +0900

    Add Redirect to Search PagE

--- web/urls.py
+++ web/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.mainPage, name='index'),
+    url(r'^search$', views.SearchPage, name='search'),
     url(r'^recipe$', views.recipePage, name='recipe'),
     url(r'^recipe/result$', views.recipeResultPage, name='recipe_result'),
     url(r'^footer/service$', views.footerProvision, name='service'),
--- web/views.py
+++ web/views.py
@@ -1,30 +1,46 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
+
+@csrf_exempt
 def mainPage(request):
     return render(request, 'web/index.html',{})
 
+@csrf_exempt
+def SearchPage(request):
+    print ("재료", request.POST)
+    return render(request, 'web/index.html', {})
+
+@csrf_exempt
 def recipePage(request):
     return render(request, 'web/recipe.html',{})
 
+@csrf_exempt
 def recipeResultPage(request):
     params = request.GET["recipe_key"]
 
+    #print (params)
+
     return render(request, 'web/recipe_result.html',{'test': params})
 
-
+@csrf_exempt
 def stuff(request):
     return render(request, 'web/stuff.html', {})
 
+@csrf_exempt
 def footerProvision(request):
     return render(request, 'web/footerProvision/provision.html',{})
 
+@csrf_exempt
 def advertising(request):
     return render(request, 'web/footerProvision/advertising.html',{})
 
+@csrf_exempt
 def faq(request):
     return render(request, 'web/footerProvision/faq.html',{})
 
+@csrf_exempt
 def complete(request):
     return render(request, 'web/footerProvision/complete.html',{})
